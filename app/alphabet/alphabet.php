97b6bc5e1bf94766b913c0b7b9f4d4c887968d71
Bill-GD <geometrydashbill@gmail.com>
2024-09-27 22:30:46 +0700

    added chars, words
    - scan & sort images (both ways)
    - print all iteratively (images files, chars & words array)
    - last item prints in center

--- app/alphabet/alphabet.php
+++ app/alphabet/alphabet.php
@@ -6,23 +6,37 @@ if (!Helper::is_user_logged_in()) {
   header('Location: ..\account\signin.php');
   exit();
 }
+
+$images = scandir('../../assets/images/alphabet');
+natsort($images);
+$images = array_values(array_diff($images, ['.', '..']));
+$chars = [
+  'A', 'Ă', 'Â', 'B', 'C', 'D', 'Đ', 'E', 'Ê', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'Ô', 'Ơ', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ư', 'V', 'X', 'Y'
+];
+$words = [
+  '魚', '月', '曇り', '飛行機', 'キツネ', 'ヤギ', '時計', '赤ちゃん', 'カエル', 'パンダ', 'イルカ', 'テレビ', 'サル', '恐竜', '蚊', 'キノコ', '牛', 'くるま', 'アボカド', '宇宙飛行士', '紙扇風機', 'ヘビ', 'ライオン', '潜水艦', 'フクロウ', '羊', 'オウム', '自転車', '看護師',
+];
+if (isset($_GET['s'])) {
+  $images = array_reverse($images);
+  $chars = array_reverse($chars);
+  $words = array_reverse($words);
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?= Helper::import_styles() ?>
     <title>アルファベット - Probeto</title>
   </head>
   <body>
     <?= UI::navbar() ?>
-
     <div class="container">
-      <div class="row flex align-items-center justify-content-between mt-7 mb-4 ms-5">
-        <p class="col-4 fs-5">アルファベット</p>
-        <div class="col-3">
+      <div class="row flex align-items-center justify-content-between mt-7 mb-4">
+        <p class="col-4 fs-4">アルファベット</p>
+        <div class="col-4">
           <div class="form-floating position-relative">
             <input type="text" class="form-control rounded-4 border-dark-subtle" id="search" name="search"
               placeholder="" required>
@@ -31,86 +45,36 @@ if (!Helper::is_user_logged_in()) {
           </div>
         </div>
         <div class="col-4 row flex justify-content-end">
-          <button class="col-auto btn btn-primary me-2" type="submit">a->z</button>
-          <button class="col-auto btn btn-primary" type="submit">z->a</button>
+          <a href="alphabet.php" class="col-auto btn btn-primary me-2">a->z</a>
+          <a href="?s" class="col-auto btn btn-primary">z->a</a>
         </div>
       </div>
     </div>
 
-    <div class="container w-75">
-      <div class="row">
-        <div class="col text-decoration-none text-center">
-          <div class="row border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
-            <div class="col-auto"><img src="../../assets/images/quick_learn/family.png" width="200" height="200"></div>
-            <div class="col-2 flex align-content-center">
-              <p class="">dasda</p>
-              <p class="">dasda</p>
-              <a class="text-decoration-none icon-link link-secondary fs-3 mt-3 fa-solid fa-volume-high"
-                id="play_sound"></a>
+    <div class="container w-75 mb-5">
+      <?php
+      for ($i = 0; $i < 29; $i++) {
+        if ($i == 0 || ($i > 1 && $i % 2 == 0)) {
+          echo '<div class="row column-gap-5 ' . ($i == 28 ? 'justify-content-center' : '') . ' ">';
+        }
+
+        echo '
+          <div class="col' . ($i == 28 ? '-6' : '') . ' text-decoration-none text-center">
+            <div class="row border border-dark-subtle rounded-1 my-2 px-3">
+              <div class="col-auto"><img src="../../assets/images/alphabet/' . $images[$i] . '" width="200" height="200"></div>
+              <div class="col-2 pt-4">
+                <p class="fw-bold fs-4">' . $chars[$i] . '</p>
+                <p>'. $words[$i] . '</p>
+                <a class="text-decoration-none icon-link link-secondary fs-3 mt-4 fa-solid fa-volume-high" id="play_sound"></a>
+              </div>
             </div>
-          </div>
-        </div>
-        <div class="col text-decoration-none text-center">
-          <div class="row border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
-            <div class="col-auto"><img src="../../assets/images/quick_learn/family.png" width="200" height="200"></div>
-            <div class="col-2 flex align-content-center">
-              <p class="">dasda</p>
-              <p class="">dasda</p>
-              <a class="text-decoration-none icon-link link-secondary fs-3 mt-3 fa-solid fa-volume-high"
-                id="play_sound"></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col text-decoration-none text-center">
-          <div class="row border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
-            <div class="col-auto"><img src="../../assets/images/quick_learn/family.png" width="200" height="200"></div>
-            <div class="col-2 flex align-content-center">
-              <p class="">dasda</p>
-              <p class="">dasda</p>
-              <a class="text-decoration-none icon-link link-secondary fs-3 mt-3 fa-solid fa-volume-high"
-                id="play_sound"></a>
-            </div>
-          </div>
-        </div>
-        <div class="col text-decoration-none text-center">
-          <div class="row border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
-            <div class="col-auto"><img src="../../assets/images/quick_learn/family.png" width="200" height="200"></div>
-            <div class="col-2 flex align-content-center">
-              <p class="">dasda</p>
-              <p class="">dasda</p>
-              <a class="text-decoration-none icon-link link-secondary fs-3 mt-3 fa-solid fa-volume-high"
-                id="play_sound"></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col text-decoration-none text-center">
-          <div class="row border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
-            <div class="col-auto"><img src="../../assets/images/quick_learn/family.png" width="200" height="200"></div>
-            <div class="col-2 flex align-content-center">
-              <p class="">dasda</p>
-              <p class="">dasda</p>
-              <a class="text-decoration-none icon-link link-secondary fs-3 mt-3 fa-solid fa-volume-high"
-                id="play_sound"></a>
-            </div>
-          </div>
-        </div>
-        <div class="col text-decoration-none text-center">
-          <div class="row border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
-            <div class="col-auto"><img src="../../assets/images/quick_learn/family.png" width="200" height="200"></div>
-            <div class="col-2 flex align-content-center">
-              <p class="">dasda</p>
-              <p class="">dasda</p>
-              <a class="text-decoration-none icon-link icon-link-hover fs-3 mt-3 fa-solid fa-volume-high"
-                id="play_sound" path=""></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+          </div>';
+
+        if ($i % 2 != 0 && $i > 0) {
+          echo '</div>';
+        }
+      }
+      ?>
     </div>
     <script>
       document.getElementById('play_sound').addEventListener('click', function () {
