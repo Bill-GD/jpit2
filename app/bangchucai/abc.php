8c637ac138f9fb0b2e3efefe1cd417ab509a3f73
vunam03 <164014080+vunam03@users.noreply.github.com>
2024-09-27 11:34:25 +0700

    12

--- app/bangchucai/abc.php
+++ app/bangchucai/abc.php
@@ -3,36 +3,117 @@ include '../helpers/helper.php';
 include '../helpers/ui.php';
 
 if (!Helper::is_user_logged_in()) {
-  header('Location: ..\account\signin.php');
-  exit();
+    header('Location: ..\account\signin.php');
+    exit();
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?= Helper::import_styles() ?>
     <title>Document</title>
-  </head>
+</head>
 
-  <body>
+<body>
     <?= UI::navbar() ?>
-    < class="container mt-7 mb-4">
-      <div>
-        <P></P>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-md-6">
-          <div class="form-floating position-relative">
-            <input type="text" class="form-control rounded-5 border-dark-subtle" id="search" name="search"
-              placeholder="検索" required>
-            <label for="search">検索</label>
-            <i class="position-absolute end-5 bottom-30 fa-solid fa-search text-grey"></i>
-          </div>
+
+    <div class="container">
+        <div class="row flex align-items-center justify-content-between mt-7 mb-4 ms-5">
+            <p class="col-4 fs-5">アルファベット</p>
+            <div class="col-3">
+                <div class="form-floating position-relative">
+                    <input type="text" class="form-control rounded-5 border-dark-subtle" id="search" name="search" placeholder=""
+                        required>
+                    <label for="search">検索</label>
+                    <i class="position-absolute end-5 bottom-35 fa-solid fa-search text-grey"></i>
+                </div>
+            </div>
+            <div class="col-4 row flex justify-content-end">
+                <button class="col-auto btn btn-primary me-2" type="submit">a->z</button>
+                <button class="col-auto btn btn-primary" type="submit">z->a</button>
+            </div>
         </div>
-      </div>
-  </body>
+    </div>
+    <div class="container w-75">
+        <div class="row">
+            <div class="col text-decoration-none text-center">
+                <div class="row border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
+                    <div class="col-auto"><img src="../../assets/images/quick_learn/family.png" width="200" height="200"></div>
+                    <div class="col-2 flex align-content-center">
+                        <p class="">dasda</p>
+                        <p class="">dasda</p>
+                        <a class="text-decoration-none icon-link icon-link-hover fs-3 mt-3 fa-solid fa-volume-high" id="play_sound" href="#"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col text-decoration-none text-center">
+                <div class="row border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
+                    <div class="col-auto"><img src="../../assets/images/quick_learn/family.png" width="200" height="200"></div>
+                    <div class="col-2 flex align-content-center">
+                        <p class="">dasda</p>
+                        <p class="">dasda</p>
+                        <a class="text-decoration-none icon-link icon-link-hover fs-3 mt-3 fa-solid fa-volume-high" id="play_sound" href="#"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col text-decoration-none text-center">
+                <div class="row border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
+                    <div class="col-auto"><img src="../../assets/images/quick_learn/family.png" width="200" height="200"></div>
+                    <div class="col-2 flex align-content-center">
+                        <p class="">dasda</p>
+                        <p class="">dasda</p>
+                        <a class="text-decoration-none icon-link icon-link-hover fs-3 mt-3 fa-solid fa-volume-high" id="play_sound" href="#"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col text-decoration-none text-center">
+                <div class="row border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
+                    <div class="col-auto"><img src="../../assets/images/quick_learn/family.png" width="200" height="200"></div>
+                    <div class="col-2 flex align-content-center">
+                        <p class="">dasda</p>
+                        <p class="">dasda</p>
+                        <a class="text-decoration-none icon-link icon-link-hover fs-3 mt-3 fa-solid fa-volume-high" id="play_sound" href="#"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col text-decoration-none text-center">
+                <div class="row border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
+                    <div class="col-auto"><img src="../../assets/images/quick_learn/family.png" width="200" height="200"></div>
+                    <div class="col-2 flex align-content-center">
+                        <p class="">dasda</p>
+                        <p class="">dasda</p>
+                        <a class="text-decoration-none icon-link icon-link-hover fs-3 mt-3 fa-solid fa-volume-high" id="play_sound" href="#"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col text-decoration-none text-center">
+                <div class="row border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
+                    <div class="col-auto"><img src="../../assets/images/quick_learn/family.png" width="200" height="200"></div>
+                    <div class="col-2 flex align-content-center">
+                        <p class="">dasda</p>
+                        <p class="">dasda</p>
+                        <a class="text-decoration-none icon-link icon-link-hover fs-3 mt-3 fa-solid fa-volume-high" id="play_sound" path="" href="#"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <script>
+        document.getElementById('play_sound').addEventListener('click', function() {
+            const audio = new Audio('../../assets/sounds/subfortopic1/a chao.m4a');
+            audio.play();
+        });
+    </script>
+</body>
+
 
 </html>
