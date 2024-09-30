a4a9ddad2571e2540231166f214538a0ffee2e62
Bill-GD <geometrydashbill@gmail.com>
2024-10-01 00:10:53 +0700

    fixed personalized layout
    - images
    - image, text alignment
    - bg, button color

--- app/personalized/personalized_plan.php
+++ app/personalized/personalized_plan.php
@@ -6,77 +6,57 @@ include_once "../helpers/ui.php";
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?= Helper::import_styles() ?>
     <title>速習画面の内容 - Probeto</title>
   </head>
-  <body class="personalize_study">
+  <body>
     <?= UI::navbar() ?>
 
-
-
     <main class="container mt-4">
-      <div class="flex align-items-center mt-7 mb-4">
+      <div class="mt-7">
         <p class="fs-5">Home > 個別化</p>
         <p class="fs-2">テストの結果に基づいて、私たちはあなたの学習プランを以下のように提案します</p>
       </div>
-      <div class="row">
-        <div class="col-md-6 mb-4">
-          <div
-            class="border border-1 border-dark-subtle rounded-2 p-3 d-flex justify-content-between align-items-center">
-            <div>
-              <h3>アルファベットと発音に慣れる</h3>
-              <p>ベトナム語のアルファベッ トを理解し、各文字の発音 を確実に把握する。</p>
-              <button class="btn btn-primary">Learn</button>
-            </div>
-            <a href="link-to-resource1.html">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/220px-Cat03.jpg"
-                alt="Description of image 1" class="img-fluid item-image" />
-            </a>
+
+      <div class="row mb-4 gap-5">
+        <div class="col d-flex flex-column justify-content-start bg-secondary-subtle rounded-2 ps-4 py-3">
+          <p class="fs-3 fw-bold">アルファベットと発音に慣れる</p>
+          <div class="row d-flex my-3">
+            <p class="col-lg-8 fs-4">ベトナム語のアルファベットを理解し、各文字の発音を確実に把握する。</p>
+            <img class="col-lg-4 rounded-4" src="../../assets/images/personalized/weird_a.jpg">
           </div>
+          <a class="btn btn-purple align-self-end mt-auto text-white px-4 py-2 rounded-5">詳細</a>
         </div>
 
-        <div class="col-md-6 mb-4">
-          <div
-            class="border border-1 border-dark-subtle rounded-2 p-3 d-flex justify-content-between align-items-center">
-            <div>
-              <h3>基本的な語彙と日常会話のフレーズを学 ぶ</h3>
-              <p>基本的な語彙や日常生活でよく 使われる会話表現を学ぶ</p>
-              <button class="btn btn-primary">Learn</button>
-            </div>
-            <a href="link-to-resource2.html">
-              <img src="path/to/image2.jpg" alt="Description of image 2" class="img-fluid item-image" />
-            </a>
+        <div class="col d-flex flex-column justify-content-start bg-secondary-subtle rounded-2 ps-4 py-3">
+          <p class="fs-3 fw-bold">基本的な語彙と日常会話のフレーズを学 ぶ</p>
+          <div class="row d-flex my-3">
+            <p class="col-lg-8 fs-4">基本的な語彙や日常生活でよく使われる会話表現を学ぶ</p>
+            <div class="col-lg-4 rounded-4"></div>
           </div>
+          <a class="btn btn-purple align-self-end mt-auto text-white px-4 py-2 rounded-5">詳細</a>
+        </div>
+      </div>
+      
+      <div class="row mb-4 gap-5">
+        <div class="col d-flex flex-column justify-content-start bg-secondary-subtle rounded-2 ps-4 py-3">
+          <p class="fs-3 fw-bold">基本的な文法を学ぶ</p>
+          <div class="row d-flex my-3">
+            <p class="col-lg-8 fs-4">ベトナム語の基本的な文法構造を理解して適用する</p>
+            <img class="col-lg-4 rounded-4" src="../../assets/images/personalized/grammar.png">
+          </div>
+          <a class="btn btn-purple align-self-end mt-auto text-white px-4 py-2 rounded-5">詳細</a>
         </div>
 
-        <div class="col-md-6 mb-4">
-          <div
-            class="border border-1 border-dark-subtle rounded-2 p-3 d-flex justify-content-between align-items-center">
-            <div>
-              <h3>基本的な文法を学ぶ</h3>
-              <p>ベトナム語の基本的な 文法構造を理解して適用 する</p>
-              <button class="btn btn-primary">Learn</button>
-            </div>
-            <a href="link-to-resource3.html">
-              <img src="path/to/image3.jpg" alt="Description of image 3" class="img-fluid item-image" />
-            </a>
+        <div class="col d-flex flex-column justify-content-start bg-secondary-subtle rounded-2 ps-4 py-3">
+          <p class="fs-3 fw-bold">会話や具体的な状況での実践練習を行う</p>
+          <div class="row d-flex my-3">
+            <p class="col-lg-8 fs-4">学んだ語彙と文法を具体的な会話の状況に応用する</p>
+            <img class="col-lg-4 rounded-4" src="../../assets/images/personalized/practice-makes-perfect-cover.png">
           </div>
-        </div>
-
-        <div class="col-md-6 mb-4">
-          <div
-            class="border border-1 border-dark-subtle rounded-2 p-3 d-flex justify-content-between align-items-center">
-            <div>
-              <h3>会話や具体的な状況での実践練習を行う</h3>
-              <p>学んだ語彙と文法を具 体的な会話の状況に応 用する</p>
-              <button class="btn btn-primary">Learn</button>
-            </div>
-            <a href="link-to-resource4.html">
-              <img src="path/to/image4.jpg" alt="Descriptiona of image 4" class="img-fluid item-image" />
-            </a>
-          </div>
+          <a class="btn btn-purple align-self-end mt-auto text-white px-4 py-2 rounded-5">詳細</a>
         </div>
       </div>
     </main>
