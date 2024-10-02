232b7c4122609d15000bc176388521f9d5c908c1
Bill-GD <geometrydashbill@gmail.com>
2024-10-02 11:55:55 +0700

    add topic image path to db

--- app/quick_learn/quick_learn_topic.php
+++ app/quick_learn/quick_learn_topic.php
@@ -5,13 +5,7 @@ include_once '../helpers/database_manager.php';
 
 $dm = DatabaseManager::instance();
 
-$topics = $dm->query('SELECT topic_name from quick_learn_topic')->fetchAll(PDO::FETCH_COLUMN);
-$images = [
-  'quick_learn/greeting.png', 'quick_learn/family.png',
-  'quick_learn/food.png', 'quick_learn/health.png',
-  'quick_learn/hobby.png', 'quick_learn/shopping.jpg',
-  'quick_learn/travel.png', 'quick_learn/work.jpg',
-];
+$topics = $dm->query('SELECT topic_name, image from quick_learn_topic')->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -49,8 +43,8 @@ $images = [
         echo '
           <a href="quick_learn_content.php?i=' . $i + 1 . '" class="col text-decoration-none text-center">
             <div class="border border-dark-subtle rounded-1 mx-1 my-2 pt-4 px-3">
-              <img src="../../assets/images/' . $images[$i] . '" width="200" height="200">
-              <p class="text-dark text-start pt-2 pb-3 ps-4 fs-4">' . $topics[$i] . '</p>
+              <img src="../../assets/images/' . $topics[$i]['image'] . '" width="200" height="200">
+              <p class="text-dark text-start pt-2 pb-3 ps-4 fs-4">' . $topics[$i]['topic_name'] . '</p>
             </div>
           </a>';
       }
