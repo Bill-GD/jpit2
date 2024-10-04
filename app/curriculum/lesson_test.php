015aa0d5cd9298a9293645ef47ff70d099d32fa2
Bill-GD <geometrydashbill@gmail.com>
2024-10-04 18:40:33 +0700

    lesson test answer feature
    - choose answer
    - can only choose 1 option
    - choosing 1 deselects others
    - check correct & calculates score
    - can only submit if answered all questions
    - random ans order

--- app/curriculum/lesson_test.php
+++ app/curriculum/lesson_test.php
@@ -45,7 +45,7 @@ foreach ($questions as $q) {
   <body>
     <?= UI::navbar() ?>
 
-    <main class="container">
+    <main class="container mb-7">
       <div class="mt-7 mb-4">
         <p class="fs-5 mt-3">
           <a class="text-decoration-none link-secondary" href="lesson_list.php">&lArr;</a>
@@ -61,19 +61,21 @@ foreach ($questions as $q) {
           $answers,
           fn($a): bool => $a['question_id'] == $q['question_id']
         ));
+        shuffle($choices);
 
         echo '
-          <div class="border border-2 rounded-3 px-3 pt-2 mb-3">
+          <div class="border border-2 rounded-3 px-3 pt-2 mb-3 question-content">
             <p class="fs-5 fw-bold">質問 ' . ($i + 1) . ':</p>
             <p class="fs-5">' . $q['content'] . '</p>
-            </div>
-            ';
+          </div>
+          ';
 
         echo '<div class="row column-gap-4 m-0 mb-5">';
         for ($j = 0; $j < count($choices); $j++) {
           echo '
-            <div class="col border border-2 rounded-3 px-3">
-              <p class="pt-2 fs-5 fw-bold">答え ' . ($j + 1) . ':</p>
+            <div class="answer-container q-' . $i . ' a-' . $j . ($choices[$j]['is_correct'] == 1 ? ' correct' : '') .
+            ' col border border-2 rounded-3">
+              <p class="pt-2 fs-5 fw-bold">答え ' . ($j + 1) . ':' . '</p>
               <p class="fs-5">' . $choices[$j]['content'] . '</p>
             </div>
           ';
@@ -88,8 +90,56 @@ foreach ($questions as $q) {
         <div class="col-6 progress rounded-4 bg-secondary-subtle p-0">
           <div class="progress-bar rounded-4 bg-primary h-100" id="progress-bar" style="width: 0%"></div>
         </div>
-        <a href="" class="col-auto btn btn-purple rounded-5 ms-5 px-3 py-2">提出</a>
+        <a class="col-auto btn btn-purple rounded-5 ms-5 px-3 py-2 disabled" id="test-submit">提出</a>
       </div>
     </div>
   </body>
+
+  <script>
+    const s = document.getElementById('test-submit');
+    const totalQuestionCount = document.getElementsByClassName('question-content').length;
+
+    s.addEventListener('click', () => {
+      if (!isAllAnswered()) return;
+
+      let correctCount = 0;
+
+      for (let i = 0; i < totalQuestionCount; i++) {
+        const answers = document.getElementsByClassName(`q-${i}`),
+          correct = Array.from(answers).filter(e => e.classList.contains('correct'))[0];
+        if (correct.classList.contains('bg-success-subtle')) {
+          correctCount++;
+        }
+      }
+
+      const score = Math.round(correctCount / totalQuestionCount * 100);
+      alert(`あなたのスコアは ${score}/100 です。`);
+    });
+
+    for (let e of document.getElementsByClassName('answer-container')) {
+      e.addEventListener('click', function () {
+        const q = e.classList[1].charAt(2), a = e.classList[2].charAt(2);
+        const count = document.getElementsByClassName(e.classList[1]).length;
+
+        const other = [];
+        for (let i = 0; i < count; i++) {
+          if (i == a) continue;
+          other.push(document.getElementsByClassName(`q-${q} a-${i}`)[0]);
+        }
+
+        if (other.some(e => e.classList.contains('bg-success-subtle'))) {
+          const chosen = other.filter(e => e.classList.contains('bg-success-subtle'))[0];
+          chosen.classList.remove('bg-success-subtle');
+        }
+
+        e.classList.add('bg-success-subtle');
+
+        if (isAllAnswered()) s.classList.remove('disabled');
+      });
+    }
+
+    function isAllAnswered() {
+      return Array.from(document.getElementsByClassName('bg-success-subtle')).length == totalQuestionCount;
+    }
+  </script>
 </html>
