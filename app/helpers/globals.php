a77d3c0288193967ad715cba6f643d2ce559f391
Bill-GD <geometrydashbill@gmail.com>
2024-10-10 18:25:40 +0700

    fixed global env on prod

--- app/helpers/globals.php
+++ app/helpers/globals.php
@@ -9,8 +9,15 @@ class Globals {
   public static string $google_client_secret = '';
 
   private static function load_env(string $env_path): void {
+    if (!file_exists($env_path)) {
+      return;
+    }
+
     $env = file($env_path);
-    // $env = explode("\n", $env);
+    if ($env === false) {
+      return;
+    }
+    
     foreach ($env as $line) {
       $line = trim($line);
       if (empty($line) || $line[0] === '#') {
@@ -22,10 +29,11 @@ class Globals {
   }
 
   public static function init(string $env_path): void {
-    if (getenv('ENVIROMENT') === false || getenv('ENVIROMENT') === 'development') {
+    $env_type = getenv('ENVIROMENT');
+    if ($env_type === false || $env_type === 'development') {
       self::load_env($env_path);
     }
-    self::$is_dev = getenv('ENVIROMENT') === 'development';
+    self::$is_dev = $env_type === false || $env_type === 'development';
     self::$aiven_username = getenv('AIVEN_USERNAME');
     self::$aiven_password = getenv('AIVEN_PASSWORD');
     self::$google_client_id = getenv('GOOGLE_CLIENT_ID');
